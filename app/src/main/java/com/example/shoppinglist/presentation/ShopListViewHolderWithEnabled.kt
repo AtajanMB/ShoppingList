0cecc30de5792f23436e327c5a795441228bca72
AtajanMB <atashmammedow20@gmail.com>
2023-12-08 17:25:15 +0500

    added recycler view supported two view holder

---
+++ app/src/main/java/com/example/shoppinglist/presentation/ShopListViewHolderWithEnabled.kt
@@ -0,0 +1,13 @@
+package com.example.shoppinglist.presentation
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.shoppinglist.R
+
+class ShopListViewHolderWithEnabled(view: View) : RecyclerView.ViewHolder(view) {
+    val tvName: TextView = view.findViewById(R.id.tv_name)
+    val tvCount: TextView = view.findViewById(R.id.tv_count)
+
+    val enabled: TextView = view.findViewById(R.id.tv_count1)
+}
